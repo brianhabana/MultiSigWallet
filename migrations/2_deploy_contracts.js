e83ae1393e2230b3b2c2efda6668aabca4dec1b9
brianhabana <brianhabana@gmail.com>
2021-11-17 19:50:14 -0800

    Update 2_deploy_contracts.js
    updated migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Wallet = artifacts.require("Wallet");
 
-module.exports = function (deployer) {
-  deployer.deploy(Wallet, [], );
+module.exports = async function (deployer, _network, accounts) {
+  await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
+  const wallet = await Wallet.deployed();
+  web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
 };
